51c6fada353c927aaff17783dc5e9bff14e4509f
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-14 05:57:18 +0330

    Update entropy.c

--- sources/entropy.c
+++ sources/entropy.c
@@ -1,1 +1,18 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+/*
+ * Read cryptographically secure random bytes
+ * from /dev/urandom.
+ */
+int sp_get_entropy(void *buffer, size_t size) {
+    int fd = open("/dev/urandom", O_RDONLY);
+    if (fd < 0)
+        return -1;
+
+    ssize_t result = read(fd, buffer, size);
+    close(fd);
+
+    return (result == (ssize_t)size) ? 0 : -1;
+}
 
