19787a48feeca84d82e44e0f3152a2f22f29542d
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-14 06:00:10 +0330

    Update test.c add quick example

--- example/test.c
+++ example/test.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include "strongpass.h"
+
+int main(void)
+{
+    char password[64];
+
+    sp_options_t options = {
+        .length = 16,
+        .use_lowercase = 1,
+        .use_uppercase = 1,
+        .use_digits = 1,
+        .use_symbols = 1
+    };
+
+    if (sp_generate(password, sizeof(password), &options) != SP_OK) {
+        printf("Password generation failed\n");
+        return 1;
+    }
+
+    printf("Generated password: %s\n", password);
+
+    sp_strength_t strength = sp_evaluate_strength(password);
+    printf("Strength: %d\n", strength);
+
+    return 0;
+}
 
